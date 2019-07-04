92e67df5fe9ac0b68230a7d69ab7348e5a7bdd30
hhotatea <hhotatea@gmail.com>
2019-07-05 01:53:21 +0900

    add audio

--- Assets/Token.cs
+++ Assets/Token.cs
@@ -8,10 +8,14 @@ public class Token : MonoBehaviour
     [SerializeField] private Vector3[] valPos = new Vector3[4];
     [SerializeField] private Material[] valMat = new Material[4];
     [SerializeField] private AudioClip[] valSe = new AudioClip[0];
+    [SerializeField] private float bmp = 4.0f;
     private Rigidbody rigidBody;
     private AudioSource audioSource;
     private Renderer meshRender;
     private int layline;
+    [SerializeField] private bool setHit = false;
+    [SerializeField] private float setSpeed = 0.1f;
+    [SerializeField] private Vector3 setPos = new Vector3(0.0f,3.0f,0.0f);
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +33,14 @@ public class Token : MonoBehaviour
     void Update()
     {
         //rigidBody.velocity = speedVec;
+        if(setHit){
+            Vector3 setVec = setPos - transform.position;
+            if(setVec.magnitude<setSpeed){
+                transform.position = setPos;
+                setHit = false;
+            }
+            transform.position = transform.position + setVec.normalized * setSpeed;
+        }
         
     }
 
@@ -42,7 +54,10 @@ public class Token : MonoBehaviour
         }else if( layerName == "hero")
         {
             Debug.Log("hit");
+            rigidBody.velocity = Vector3.zero;
             audioSource.Play ();
+            setPos = new Vector3( ((Time.time%bmp)/bmp-0.5f)*setPos.x, setPos.y,setPos.z);
+            setHit = true;
         }else if( layerName == "trig")
         {
             audioSource.Play ();
---
+++ Assets/spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    [SerializeField] private float minTime = 0.0f;
+    [SerializeField] private float maxTime = 5.0f;
+    [SerializeField] private GameObject plefabSample;
+    private float intervalTime = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        intervalTime = Random.Range(minTime,maxTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        intervalTime -= Time.deltaTime;
+        if(intervalTime<0.0f){
+            Instantiate (plefabSample,transform);
+            intervalTime = Random.Range(minTime,maxTime);
+        }
+    }
+}
